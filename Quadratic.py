f0a043172fff528e05ccd2464c1cb539b4a2251c
Eloviyo <shirajum.monira@helsinki.fi>
2020-04-26 22:00:35 +0300

    fixed comment

--- Quadratic.py
+++ Quadratic.py
@@ -11,7 +11,7 @@ a = float(input('Enter a: '))
 b = float(input('Enter b: '))
 c = float(input('Enter c: '))
 
-d = b**2-4*a*c #calculating determinant
+d = b**2-4*a*c #calculating discriminant
 
 #vertex of a parabola gives highest and lowest points on it
 x_ver = -b/(2*a)
